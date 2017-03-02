5ac3da94085517520a4528ef911419c668700c17
hobiana <hobianarazakanaivo@gmail.com>
2017-03-02 14:28:40 +0300

    2 commit

--- insert.sql
+++ insert.sql
@@ -1,22 +1,22 @@
-INSERT INTO employe (idemploye, idposte, nom, cin, matricule, salairebase, dateembauche) VALUES (1, 1, 'Razakanaivo Hobiana', '111111111111', 'EMP00001', 5000000.00,'2016-12-01');
-INSERT INTO employe (idemploye, idposte, nom, cin, matricule, salairebase, dateembauche) VALUES (2, 1, 'Rabe Jean', '222222222222', 'EMP00002', 1000000.00,'2016-12-01');
-INSERT INTO employe (idemploye, idposte, nom, cin, matricule, salairebase, dateembauche) VALUES (4, 3, 'Rabetrano Benja', '789632587415', 'EMP00004', 900000.00,'2016-12-01');
-INSERT INTO employe (idemploye, idposte, nom, cin, matricule, salairebase, dateembauche) VALUES (6, 2, 'Rabenarivo Amir', '789630145796', 'EMP00006', 950000.00,'2016-12-01');
-INSERT INTO employe (idemploye, idposte, nom, cin, matricule, salairebase, dateembauche) VALUES (5, 3, 'Jean de Dieu', '523698521478', 'EMP00005', 950000.00,'2016-12-01');
-INSERT INTO employe (idemploye, idposte, nom, cin, matricule, salairebase, dateembauche) VALUES (3, 3, 'Rakoto Solofo', '457896321457', 'EMP00003', 950000.00,'2016-12-01');
-INSERT INTO employe (idemploye, idposte, nom, cin, matricule, salairebase, dateembauche) VALUES (7, 4, 'Herinihaja Sitrakiniaina', '123654789655', 'EMP00007', 1000000.00,'2016-12-01');
-INSERT INTO employe (idemploye, idposte, nom, cin, matricule, salairebase, dateembauche) VALUES (9, 5, 'Aiglon Mamy', '632578941258', 'EMP00009', 980000.00,'2016-12-01');
-INSERT INTO employe (idemploye, idposte, nom, cin, matricule, salairebase, dateembauche) VALUES (8, 5, 'Nambinintsoa Namby', '745698236547', 'EMP00008', 970000.00,'2016-12-01');
-INSERT INTO employe (idemploye, idposte, nom, cin, matricule, salairebase, dateembauche) VALUES (10, 2, 'Ratefinjanahary Michael', '578964128795', 'EMP00010', 1000000.00,'2016-12-01');
-INSERT INTO employe (idemploye, idposte, nom, cin, matricule, salairebase, dateembauche) VALUES (11, 3, 'Rasoa Adele', '457879632147', 'EMP00011', 950000.00,'2016-12-01');
-INSERT INTO employe (idemploye, idposte, nom, cin, matricule, salairebase, dateembauche) VALUES (12, 5, 'Andriamparana Volana', '693258741575', 'EMP00012', 1200000.00,'2016-12-01');
-INSERT INTO employe (idemploye, idposte, nom, cin, matricule, salairebase, dateembauche) VALUES (13, 6, 'Rakoto Tasovy', '478965147856', 'EMP00013', 1200000.00,'2016-12-01');
-INSERT INTO employe (idemploye, idposte, nom, cin, matricule, salairebase, dateembauche) VALUES (14, 7, 'Telolahy Lanto', '753951478963', 'EMP00014', 800000.00,'2016-12-01');
-INSERT INTO employe (idemploye, idposte, nom, cin, matricule, salairebase, dateembauche) VALUES (19, 7, 'Rasolofo Tiana', '456987123458', 'EMP00015', 800000.00,'2016-12-01');
-INSERT INTO employe (idemploye, idposte, nom, cin, matricule, salairebase, dateembauche) VALUES (20, 7, 'Ravoay Kevin', '452136985476', 'EMP00016', 800000.00,'2016-12-01');
-INSERT INTO employe (idemploye, idposte, nom, cin, matricule, salairebase, dateembauche) VALUES (21, 2, 'Ravahiny Liva', '452136985457', 'EMP00017', 1000000.00,'2016-12-01');
-INSERT INTO employe (idemploye, idposte, nom, cin, matricule, salairebase, dateembauche) VALUES (22, 4, 'Andriambololona Kanto', '589632147575', 'EMP00018', 1000000.00,'2016-12-01');
-INSERT INTO employe (idemploye, idposte, nom, cin, matricule, salairebase, dateembauche) VALUES (32, 1, 'Ra Rafidy', '452136978545', 'EMP00098', 1000000.00,'2016-12-01');
+INSERT INTO employe (idemploye,  nom, cin, matricule, salairebase, dateembauche) VALUES (1, 'Razakanaivo Hobiana', '111111111111', 'EMP00001', 5000000.00,'2016-12-01');
+INSERT INTO employe (idemploye,  nom, cin, matricule, salairebase, dateembauche) VALUES (2, 'Rabe Jean', '222222222222', 'EMP00002', 1000000.00,'2016-12-01');
+INSERT INTO employe (idemploye,  nom, cin, matricule, salairebase, dateembauche) VALUES (4, 'Rabetrano Benja', '789632587415', 'EMP00004', 900000.00,'2016-12-01');
+INSERT INTO employe (idemploye,  nom, cin, matricule, salairebase, dateembauche) VALUES (6,  'Rabenarivo Amir', '789630145796', 'EMP00006', 950000.00,'2016-12-01');
+INSERT INTO employe (idemploye,  nom, cin, matricule, salairebase, dateembauche) VALUES (5,  'Jean de Dieu', '523698521478', 'EMP00005', 950000.00,'2016-12-01');
+INSERT INTO employe (idemploye,  nom, cin, matricule, salairebase, dateembauche) VALUES (3,  'Rakoto Solofo', '457896321457', 'EMP00003', 950000.00,'2016-12-01');
+INSERT INTO employe (idemploye,  nom, cin, matricule, salairebase, dateembauche) VALUES (7,  'Herinihaja Sitrakiniaina', '123654789655', 'EMP00007', 1000000.00,'2016-12-01');
+INSERT INTO employe (idemploye,  nom, cin, matricule, salairebase, dateembauche) VALUES (9,  'Aiglon Mamy', '632578941258', 'EMP00009', 980000.00,'2016-12-01');
+INSERT INTO employe (idemploye,  nom, cin, matricule, salairebase, dateembauche) VALUES (8, 'Nambinintsoa Namby', '745698236547', 'EMP00008', 970000.00,'2016-12-01');
+INSERT INTO employe (idemploye,  nom, cin, matricule, salairebase, dateembauche) VALUES (10, 'Ratefinjanahary Michael', '578964128795', 'EMP00010', 1000000.00,'2016-12-01');
+INSERT INTO employe (idemploye, nom, cin, matricule, salairebase, dateembauche) VALUES (11,  'Rasoa Adele', '457879632147', 'EMP00011', 950000.00,'2016-12-01');
+INSERT INTO employe (idemploye,  nom, cin, matricule, salairebase, dateembauche) VALUES (12,  'Andriamparana Volana', '693258741575', 'EMP00012', 1200000.00,'2016-12-01');
+INSERT INTO employe (idemploye,  nom, cin, matricule, salairebase, dateembauche) VALUES (13,  'Rakoto Tasovy', '478965147856', 'EMP00013', 1200000.00,'2016-12-01');
+INSERT INTO employe (idemploye,  nom, cin, matricule, salairebase, dateembauche) VALUES (14,  'Telolahy Lanto', '753951478963', 'EMP00014', 800000.00,'2016-12-01');
+INSERT INTO employe (idemploye,  nom, cin, matricule, salairebase, dateembauche) VALUES (19, 'Rasolofo Tiana', '456987123458', 'EMP00015', 800000.00,'2016-12-01');
+INSERT INTO employe (idemploye,  nom, cin, matricule, salairebase, dateembauche) VALUES (20,  'Ravoay Kevin', '452136985476', 'EMP00016', 800000.00,'2016-12-01');
+INSERT INTO employe (idemploye,  nom, cin, matricule, salairebase, dateembauche) VALUES (21,  'Ravahiny Liva', '452136985457', 'EMP00017', 1000000.00,'2016-12-01');
+INSERT INTO employe (idemploye,  nom, cin, matricule, salairebase, dateembauche) VALUES (22,  'Andriambololona Kanto', '589632147575', 'EMP00018', 1000000.00,'2016-12-01');
+INSERT INTO employe (idemploye,  nom, cin, matricule, salairebase, dateembauche) VALUES (32,  'Ra Rafidy', '452136978545', 'EMP00098', 1000000.00,'2016-12-01');
 
 
 --
